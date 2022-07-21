8d3f0c9c99e74e2b10429a99efa1e126899dc05d
Akshita Gupta <72850923+akshitagupta2@users.noreply.github.com>
2022-07-21 08:34:12 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+/*
+Given an array of integers arr[] of size N and an integer, 
+the task is to rotate the array elements to the left by d positions.
+Input: 
+arr[] = {1, 2, 3, 4, 5, 6, 7}, d = 2
+Output: 3 4 5 6 7 1 2
+
+Input: arr[] = {3, 4, 5, 6, 7, 1, 2}, d=2
+Output: 5 6 7 1 2 3 4 
+*/
